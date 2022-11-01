78ebc364a59ec0f6a8ca51a998ef447af3368532
Dmitry <di.mesler.2010@yandex.ru>
2022-11-01 16:07:44 +0300

    REFACTORING: brain_even game

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,19 +1,15 @@
 from random import randint
 
-
 import prompt
-
 
 from brain_games.cli import lose_message
 
 
 def even(name):
     min_number, max_number = 1, 399
-    print(f'Question {(number := randint(min_number, max_number))}')
+    print(f'Question: {(number := randint(min_number, max_number))}')
     answer = prompt.string('Your answer: ')
-    if answer == (correct_answer := ('yes', 'no')[number % 2]):
-        print('Correct!')
-    else:
+    if answer != (correct_answer := ('yes', 'no')[number % 2]):
         lose_message(
             answer=answer,
             correct_answer=correct_answer,
