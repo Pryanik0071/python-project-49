eaa03688aefe1d6c879ff6469b6b3697e8ef6a66
Dmitry <di.mesler.2010@yandex.ru>
2022-11-07 15:16:45 +0300

    refactoring: brain-even game

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,18 +1,11 @@
 import prompt
 
-from brain_games.cli import lose_message, question_message
-from brain_games.funcs import get_random_number
+from brain_games.cli import question_message
+from brain_games.funcs import get_random_number, check_answer
 
 
-def even(name):
+def even():
     number = get_random_number()
     question_message(number)
     answer = prompt.string('Your answer: ')
-    if answer != (correct_answer := ('yes', 'no')[number % 2]):
-        lose_message(
-            answer=answer,
-            correct_answer=correct_answer,
-            name=name
-        )
-        return
-    return True
+    return check_answer(answer, ('yes', 'no')[number % 2])
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,8 +1,10 @@
-from brain_games.cli import run_game
+#!/usr/bin/env python3
+from brain_games.funcs import run_game
 from brain_games.games.even import even
 
 
 def main():
+    """brain-even game"""
     run_game(
         'Answer "yes" if the number is even, otherwise answer "no".',
         even
