b5d2812d9ba6a911be8f4cebfb572f0449d3d3ba
Dmitry <di.mesler.2010@yandex.ru>
2022-11-07 15:16:07 +0300

    feat: add funcs - check answer & run game

--- brain_games/funcs.py
+++ brain_games/funcs.py
@@ -1,6 +1,39 @@
 from random import randint
 
+from brain_games.cli import welcome_user, lose_message
+
+
+MIN_NUMBER = 1
+MAX_NUMBER = 100
+ROUNDS_NUMBER = 3
+
 
 def get_random_number():
-    min_number, max_number = 1, 100
-    return randint(min_number, max_number)
+    return randint(MIN_NUMBER, MAX_NUMBER)
+
+
+def check_answer(answer, correct_answer):
+    if answer != correct_answer:
+        return {
+            'status': False,
+            'answer': answer,
+            'correct_answer': correct_answer
+        }
+    return {'status': True}
+
+
+def run_game(game_message, game):
+    name = welcome_user()
+    print(game_message)
+    for _ in range(ROUNDS_NUMBER):
+        status = game()
+        if not status.get('status'):
+            lose_message(
+                answer=status.get('answer'),
+                correct_answer=status.get('correct_answer'),
+                name=name
+            )
+            break
+        print('Correct!')
+    else:
+        print(f'Congratulations, {name}!')
