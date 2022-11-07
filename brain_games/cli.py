6afebacd19e8d49b6ca74722502ff71972ce9075
Dmitry <di.mesler.2010@yandex.ru>
2022-11-07 15:17:25 +0300

    refactoring: run game, delete from cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -8,23 +8,6 @@ def welcome_user():
     return name
 
 
-def run_game(game_message, game):
-    name = welcome_user()
-    rounds_number = 3
-    print(game_message)
-    for _ in range(rounds_number):
-        if not game(name):
-            # lose_message(
-            #     answer=answer,
-            #     correct_answer=correct_answer,
-            #     name=name
-            # )
-            break
-        print('Correct!')
-    else:
-        print(f'Congratulations, {name}!')
-
-
 def lose_message(answer, correct_answer, name):
     print(f"'{answer}' is wrong answer ;(. "
           f"Correct answer was '{correct_answer}'.")
