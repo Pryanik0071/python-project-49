7d32c4cfcee4013591f2558881aec9c8f59bc8d9
Dmitry <di.mesler.2010@yandex.ru>
2022-11-18 14:19:13 +0300

    refactoring: core.py - move print messages to run_game function.

--- brain_games/core.py
+++ brain_games/core.py
@@ -1,25 +1,22 @@
-from brain_games import cli
+import prompt
 
 
 NUMBER_OF_ROUNDS = 3
 
 
 def run_game(game):
-    name = cli.welcome_user()
-    status = True
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
     print(game.RULE)
     for _ in range(NUMBER_OF_ROUNDS):
         question, correct_answer = game.get_question_and_correct_answer()
         print(f'Question: {question}')
-        if not (status := is_correct_answer(correct_answer)):
+        if (answer := prompt.string('Your answer: ')) != correct_answer:
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'.\n"
+                  f"Let's try again, {name}!")
             break
         print('Correct!')
-    cli.end_game_message(status, name)
-
-
-def is_correct_answer(correct_answer: str) -> True or False:
-    """Check answer with correct_answer and return True or False"""
-    if (answer := cli.answer_message()) != correct_answer:
-        cli.wrong_answer_message(answer, correct_answer)
-        return False
-    return True
+    else:
+        print(f'Congratulations, {name}!')
