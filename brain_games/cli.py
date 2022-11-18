37ad4213cc53d65dfe17fdce649bd8815d875aff
Dmitry <di.mesler.2010@yandex.ru>
2022-11-18 14:20:05 +0300

    refactoring: cli.py - include only welcome_user() func

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -5,20 +5,3 @@ def welcome_user():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-    return name
-
-
-def wrong_answer_message(answer, correct_answer):
-    print(f"'{answer}' is wrong answer ;(. "
-          f"Correct answer was '{correct_answer}'.")
-
-
-def end_game_message(status: bool, name: str):
-    if status:
-        print(f"Congratulations, {name}!")
-        return
-    print(f"Let's try again, {name}!")
-
-
-def answer_message():
-    return prompt.string('Your answer: ')
